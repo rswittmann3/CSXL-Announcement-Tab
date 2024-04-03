2583d95f4530f7b553047aceffef52cf5b2cdc74
Aditya Krishna <51248874+adityakris03@users.noreply.github.com>
2024-04-02 23:56:03 -0400

    added frontend component (#8)

---
+++ frontend/src/app/announcement/announcement.model.ts
@@ -0,0 +1,13 @@
+export interface Announcement {
+  id: number | null;
+  author: string;
+  organization: string | null;
+  slug: string;
+  img: string | null;
+  published_date: string;
+  modified_date: string;
+  headline: string;
+  synopsis: string;
+  main_story: string;
+  state: string;
+}
