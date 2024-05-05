5a9023f72e06843e00a5adadf472b1f8055a3e66
Robert Wittmann <rwittmann@unc.edu>
2024-05-05 13:11:53 -0400

    Revert "fixed reset_demo"
    This reverts commit fbacb80026599d815906e3209ba60e454ac79160.

--- backend/test/services/role_data.py
+++ backend/test/services/role_data.py
@@ -20,7 +20,7 @@ root_role = Role(id=1, name="root")
 ambassador_role = Role(id=2, name="ambassadors")
 org_leader_role = Role(id=4, name="org_leaders")
 
-roles = [root_role, ambassador_role, org_leader_role]
+roles = [root_role, ambassador_role]
 
 
 def insert_fake_data(session: Session):
